83c66d26ed03c477c245327ac3a7774bc31ad1de
gökhan anıl koç <anil.koc@detaysoft.com>
2021-09-06 17:05:38 +0300

    replace added for jenkinsfile

--- ASPNETCORE-Sample-For-Jenkins/create-mvc-sample.bat
+++ ASPNETCORE-Sample-For-Jenkins/create-mvc-sample.bat
@@ -2,7 +2,7 @@
 :thnx: https://stackoverflow.com/questions/2541767/what-is-the-proper-way-to-test-if-a-parameter-is-empty-in-a-batch-file
 SET "dockerhub=%~1"
 SET "param2=%~2"
-setlocal EnableDelayedExpansion
+SETLOCAL EnableDelayedExpansion
 IF "!dockerhub!"=="" (
 IF "!param2!"=="" (
 SET /p dockerhub="Enter your dockerhub repository name for docker-compose: "
@@ -11,8 +11,7 @@ RMDIR sample-mvc /S/Q
 dotnet new mvc --language C# --output sample-mvc\sample-mvc --name sample-mvc -f net5.0
 dotnet add sample-mvc\sample-mvc package Microsoft.VisualStudio.Azure.Containers.Tools.Targets --version 1.11.1
 PowerShell.exe -Command "& {(Get-Content 'files\docker-compose.yml').replace('<image>', '!dockerhub!') | Set-Content 'sample-mvc/docker-compose.yml'}"
-ENDLOCAL
+PowerShell.exe -Command "& {(Get-Content 'files/Jenkinsfile').replace('<dockerhub>', '!dockerhub!') | Set-Content 'sample-mvc/Jenkinsfile'}"
 XCOPY files\.dockerignore sample-mvc /Y
 XCOPY files\Dockerfile sample-mvc /Y
-XCOPY files\jenkinsfile sample-mvc /Y
 curl -o sample-mvc/.gitignore https://raw.githubusercontent.com/github/gitignore/master/VisualStudio.gitignore --ssl-no-revoke
